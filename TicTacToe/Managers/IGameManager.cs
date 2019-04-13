51b90c4113207bbedd6a394546ab04a708eaa0cf
Danyelaristizabal <Danyelaristizabal@hotmail.com>
2019-04-13 08:33:40 +0300

    Added, implemented GameManager interface

---
+++ TicTacToe/Managers/IGameManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace TicTacToe.Managers
+{
+    public interface IGameManager
+    {
+        Game MyGame { get; set; }
+       bool Over { get; set; }
+       bool Draw { get; set; }
+    }
+}
